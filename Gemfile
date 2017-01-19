bab82f59f53381a4f7ac0c2b2b77faa81aa49f79
michaelwishlow <michael@pantheritconsulting.com>
2017-01-19 14:12:01 -0500

    Inital Models/Controllers/Simple Form

--- Gemfile
+++ Gemfile
@@ -23,6 +23,14 @@ gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
 
+gem 'haml', '~> 4.0', '>= 4.0.5'
+
+gem 'simple_form', '~> 3.0', '>= 3.0.2'
+
+gem 'devise', '~> 3.4', '>= 3.4.1'
+
+gem 'paperclip', '~> 4.2'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,44 @@
+class PostsController < ApplicationController
+
+	def index
+	end
+
+	def show
+		@post = Post.all
+	end
+
+	def new
+		@post = Post.new
+	end
+
+	def create
+		@post = Post.new(post_params)
+
+		if @post.save
+			redirect_to @post
+		else
+			render 'new'
+		end
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+	def destroy
+	end 
+
+	private
+
+	def find_post
+	end
+
+
+	def post_params
+		params.require(:post).permit(:title, :link, :description)
+	end
+
+	
+end
