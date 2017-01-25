800fd6e6de4c22c4918d2f7ab0a15e83c363415f
michaelwishlow <michael@pantheritconsulting.com>
2017-01-25 14:10:51 -0500

    Added Paperclip

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -48,7 +48,7 @@ class PostsController < ApplicationController
 
 
 	def post_params
-		params.require(:post).permit(:title, :link, :description )
+		params.require(:post).permit(:title, :link, :description, :image )
 	end
 
 	
